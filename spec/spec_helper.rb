c15ab21af09600dd8c4cf2dbfb30a22510bfc13d
bikash <bikash@sprout-technology.com>
2011-12-08 16:17:25 +0545

    filter run added

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,6 +18,8 @@ RSpec.configure do |config|
 
   config.filter_run :focus => true
 
+  config.run_all_when_everything_filtered = true
+
   config.treat_symbols_as_metadata_keys_with_true_values = true
 
   config.infer_base_class_for_anonymous_controllers = false
